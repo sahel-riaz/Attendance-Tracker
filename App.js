b701f1338c08e1f86205ac0efb06fa246e376d40
Aadhavan <aadhavanlenin@gmail.com>
2023-02-18 16:04:29 +0530

    local storage working

--- App.js
+++ App.js
@@ -1,11 +1,71 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { useEffect, useState } from 'react'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default function App() {
+	const [data, setData] = useState()
+
+	async function handlePress() {
+		let temp = data
+		temp[0]['CS3004D'][0]['classes'][0]['CS01'][0]['date'].push('23/01/23')
+		setData(temp)
+		const temp_json = JSON.stringify(temp)
+		await AsyncStorage.setItem('data', temp_json)
+	}
+
+	// async function test() {
+	// 	try {
+	// 		const temp = [
+	// 			{
+	// 				CS3004D: [
+	// 					{
+	// 						course_name: 'Software Enginnering',
+	// 						classes: [
+	// 							{
+	// 								CS01: [
+	// 									{
+	// 										date: ['17/01/2023', '18/02/2023'],
+	// 									},
+	// 								],
+	// 							},
+	// 						],
+	// 					},
+	// 				],
+	// 			},
+	// 		]
+	// 		const temp_json = JSON.stringify(temp)
+	// 		await AsyncStorage.setItem('data', temp_json)
+	// 	} catch (error) {
+	// 		//
+	// 	}
+	// }
+
+	async function yes() {
+		try {
+			const hug = await AsyncStorage.getItem('data')
+			// console.log(JSON.parse(hug))
+			setData(JSON.parse(hug))
+			console.log(data[0]?.['CS3004D'][0]['classes'][0]['CS01'][0]['date'])
+		} catch (error) {
+			//
+		}
+	}
+
 	return (
 		<View style={styles.container}>
 			<Text>Open up App.js to start working on your app!</Text>
 			<StatusBar style='auto' />
+			{/* <TextInput
+				style={styles.input}
+				onChange={(newText) => setText(newText)}
+				value={text}
+				placeholder='useless placeholder'
+			/> */}
+			{/* <Text>{data[0]?.CS3004D[0].course_name}</Text> */}
+			{/* <Button onPress={test} title='press' /> */}
+			<Button onPress={yes} title='press' />
+			<Button onPress={handlePress} title='press' />
 		</View>
 	)
 }
@@ -17,4 +77,59 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
+	input: {
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		padding: 10,
+	},
 })
+
+// [
+// 	{
+// 		CS3004D: [
+// 			{
+// 				course_name: 'Software Enginnering',
+// 				classes: {
+// 					CS01: [
+// 						{
+// 							date: ['17/02/2023', '18/02/2023', '19/02/2023', '20/02/2023', '21/02/2023'],
+// 							students: [
+// 								{
+// 									name: 'Aadhavan Paavai Lenin',
+// 									roll_number: 'B200046CS',
+// 									attendance: [0, 0, 0, 0, 0],
+// 								},
+// 								{ name: 'Anagh Deepak', roll_number: 'B200048CS', attendance: [0, 0, 0, 0, 0] },
+// 								{ name: 'Chacko James', roll_number: 'B200017CS', attendance: [0, 0, 0, 0, 0] },
+// 							],
+// 						},
+// 					],
+// 				},
+// 			},
+// 		],
+// 	},
+// 	{
+// 		CS3002D: [
+// 			{
+// 				course_name: 'Compiler Design',
+// 				classes: {
+// 					CS01: [
+// 						{
+// 							date: ['17/02/2023', '18/02/2023', '19/02/2023', '20/02/2023', '21/02/2023'],
+// 							students: [
+// 								{
+// 									name: 'Aadhavan Paavai Lenin',
+// 									roll_number: 'B200046CS',
+// 									attendance: [0, 0, 0, 0, 0],
+// 								},
+// 								{ name: 'Anagh Deepak', roll_number: 'B200048CS', attendance: [0, 0, 0, 0, 0] },
+// 								{ name: 'Chacko James', roll_number: 'B200017CS', attendance: [0, 0, 0, 0, 0] },
+// 							],
+// 						},
+// 					],
+// 				},
+// 			},
+// 		],
+// 	},
+// ],
