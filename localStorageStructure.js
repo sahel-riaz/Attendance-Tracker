d40c09449627f3722a8b076f6fdcfad3d992cb22
Aadhavan <aadhavanlenin@gmail.com>
2023-02-25 00:39:07 +0530

    local storage structure change

--- localStorageStructure.js
+++ localStorageStructure.js
@@ -1,156 +1,68 @@
-const data = [
-	{
-		CS4001D: [
-			{ courseName: 'Software Enginnering' },
-			{
-				classes: [
+const data = {
+	CS4001D: {
+		courseName: 'Software Enginnering',
+		classes: {
+			CS01: {
+				date: ['12/02/23', '13/02/23', '14/02/23'],
+				students: [
 					{
-						CS01: [
-							{ date: ['12/02/23', '13/02/23', '14/02/23'] },
-							{
-								students: [
-									[
-										{ studentName: 'Aadhavan Paavai Lenin' },
-										{ studentID: 'B200046CS' },
-										{ attendance: [0, 1, 2] },
-									],
-									[
-										{ studentName: 'Anagh Deepak' },
-										{ studentID: 'B200048CS' },
-										{ attendance: [1, 2, 0] },
-									],
-									[
-										{ studentName: 'Chacko James' },
-										{ studentID: 'B200017CS' },
-										{ attendance: [1, 1, 2] },
-									],
-									[
-										{ studentName: 'Madhav Prabhu' },
-										{ studentID: 'B200738CS' },
-										{ attendance: [1, 1, 1] },
-									],
-									[
-										{ studentName: 'Sahel N Riaz' },
-										{ studentID: 'B200058CS' },
-										{ attendance: [0, 0, 0] },
-									],
-								],
-							},
-						],
+						studentName: 'Aadhavan',
+						studentID: 'B200046CS',
+						attendance: [0, 1, 2],
 					},
-					{
-						CS01: [
-							{ date: ['12/02/23', '13/02/23', '14/02/23'] },
-							{
-								students: [
-									[
-										{ studentName: 'Aadhavan Paavai Lenin' },
-										{ studentID: 'B200046CS' },
-										{ attendance: [0, 1, 2] },
-									],
-									[
-										{ studentName: 'Anagh Deepak' },
-										{ studentID: 'B200048CS' },
-										{ attendance: [1, 2, 0] },
-									],
-									[
-										{ studentName: 'Chacko James' },
-										{ studentID: 'B200017CS' },
-										{ attendance: [1, 1, 2] },
-									],
-									[
-										{ studentName: 'Madhav Prabhu' },
-										{ studentID: 'B200738CS' },
-										{ attendance: [1, 1, 1] },
-									],
-									[
-										{ studentName: 'Sahel N Riaz' },
-										{ studentID: 'B200058CS' },
-										{ attendance: [0, 0, 0] },
-									],
-								],
-							},
-						],
-					},
+					{ studentName: 'Anagh Deepak', studentID: 'B200048CS', attendance: [1, 2, 0] },
+					{ studentName: 'Chacko James', studentID: 'B200017CS', attendance: [1, 1, 2] },
+					{ studentName: 'Madhav Prabhu', studentID: 'B200738CS', attendance: [1, 1, 1] },
+					{ studentName: 'Sahel N Riaz', studentID: 'B200056CS', attendance: [0, 0, 0] },
 				],
 			},
-		],
+		},
+		CS02: {
+			date: ['12/02/23', '13/02/23', '14/02/23'],
+			students: [
+				[
+					{ studentName: 'Aadhavan Paavai Lenin' },
+					{ studentID: 'B200046CS' },
+					{ attendance: [0, 1, 2] },
+				],
+				[{ studentName: 'Anagh Deepak' }, { studentID: 'B200048CS' }, { attendance: [1, 2, 0] }],
+				[{ studentName: 'Chacko James' }, { studentID: 'B200017CS' }, { attendance: [1, 1, 2] }],
+				[{ studentName: 'Madhav Prabhu' }, { studentID: 'B200738CS' }, { attendance: [1, 1, 1] }],
+				[{ studentName: 'Sahel N Riaz' }, { studentID: 'B200058CS' }, { attendance: [0, 0, 0] }],
+			],
+		},
 	},
-	{
-		CS3120D: [
-			{ courseName: 'Compiler Design' },
-			{
-				classes: [
-					{
-						CS01: [
-							{ date: ['12/02/23', '13/02/23', '14/02/23'] },
-							{
-								students: [
-									[
-										{ studentName: 'Aadhavan Paavai Lenin' },
-										{ studentID: 'B200046CS' },
-										{ attendance: [0, 1, 2] },
-									],
-									[
-										{ studentName: 'Anagh Deepak' },
-										{ studentID: 'B200048CS' },
-										{ attendance: [1, 2, 0] },
-									],
-									[
-										{ studentName: 'Chacko James' },
-										{ studentID: 'B200017CS' },
-										{ attendance: [1, 1, 2] },
-									],
-									[
-										{ studentName: 'Madhav Prabhu' },
-										{ studentID: 'B200738CS' },
-										{ attendance: [1, 1, 1] },
-									],
-									[
-										{ studentName: 'Sahel N Riaz' },
-										{ studentID: 'B200058CS' },
-										{ attendance: [0, 0, 0] },
-									],
-								],
-							},
-						],
-					},
-					{
-						CS01: [
-							{ date: ['12/02/23', '13/02/23', '14/02/23'] },
-							{
-								students: [
-									[
-										{ studentName: 'Aadhavan Paavai Lenin' },
-										{ studentID: 'B200046CS' },
-										{ attendance: [0, 1, 2] },
-									],
-									[
-										{ studentName: 'Anagh Deepak' },
-										{ studentID: 'B200048CS' },
-										{ attendance: [1, 2, 0] },
-									],
-									[
-										{ studentName: 'Chacko James' },
-										{ studentID: 'B200017CS' },
-										{ attendance: [1, 1, 2] },
-									],
-									[
-										{ studentName: 'Madhav Prabhu' },
-										{ studentID: 'B200738CS' },
-										{ attendance: [1, 1, 1] },
-									],
-									[
-										{ studentName: 'Sahel N Riaz' },
-										{ studentID: 'B200058CS' },
-										{ attendance: [0, 0, 0] },
-									],
-								],
-							},
-						],
-					},
+	CS3423D: {
+		courseName: 'Software Enginnering',
+		classes: {
+			CS01: {
+				date: ['12/02/23', '13/02/23', '14/02/23'],
+				students: [
+					[
+						{ studentName: 'Aadhavan Paavai Lenin' },
+						{ studentID: 'B200046CS' },
+						{ attendance: [0, 1, 2] },
+					],
+					[{ studentName: 'Anagh Deepak' }, { studentID: 'B200048CS' }, { attendance: [1, 2, 0] }],
+					[{ studentName: 'Chacko James' }, { studentID: 'B200017CS' }, { attendance: [1, 1, 2] }],
+					[{ studentName: 'Madhav Prabhu' }, { studentID: 'B200738CS' }, { attendance: [1, 1, 1] }],
+					[{ studentName: 'Sahel N Riaz' }, { studentID: 'B200058CS' }, { attendance: [0, 0, 0] }],
 				],
 			},
-		],
+		},
+		CS02: {
+			date: ['12/02/23', '13/02/23', '14/02/23'],
+			students: [
+				[
+					{ studentName: 'Aadhavan Paavai Lenin' },
+					{ studentID: 'B200046CS' },
+					{ attendance: [0, 1, 2] },
+				],
+				[{ studentName: 'Anagh Deepak' }, { studentID: 'B200048CS' }, { attendance: [1, 2, 0] }],
+				[{ studentName: 'Chacko James' }, { studentID: 'B200017CS' }, { attendance: [1, 1, 2] }],
+				[{ studentName: 'Madhav Prabhu' }, { studentID: 'B200738CS' }, { attendance: [1, 1, 1] }],
+				[{ studentName: 'Sahel N Riaz' }, { studentID: 'B200058CS' }, { attendance: [0, 0, 0] }],
+			],
+		},
 	},
-]
+}
