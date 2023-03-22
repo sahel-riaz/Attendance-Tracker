f5175166db09e7a4ce46e83ec19f179ca83873e2
sahelriaz <sahelnriaz@gmail.com>
2023-03-22 19:45:11 +0530

    Buttons added

--- components/home/ErrorPopUp.js
+++ components/home/ErrorPopUp.js
@@ -1,6 +1,7 @@
 import { View, Text, Image } from 'react-native'
 import { Path, Svg } from 'react-native-svg'
 import { COLORS, FONTS } from '../../styles/theme'
+import { TouchableOpacity } from 'react-native'
 
 export default function ErrorPopUp() {
   return (
@@ -10,7 +11,7 @@ export default function ErrorPopUp() {
         height : 184,
         backgroundColor : 'white',
         marginTop : 150,
-        marginLeft : 30,
+        marginLeft : 35,
         borderRadius : 20,
     }}>
 
@@ -20,7 +21,7 @@ export default function ErrorPopUp() {
             alignItems : 'center',
         }}>
             <Text style = {{
-                fontFamily: FONTS?.regular, //once check
+                fontFamily: FONTS?.bold, //once check
                 fontWeight : 600,
                 fontSize : 26,
                 lineHeight : 34
@@ -41,6 +42,53 @@ export default function ErrorPopUp() {
                     Are you sure you want to delete this session?
                 </Text>
             </View>
+        </View>
+        {/* Buttons */}
+        <View style = {{
+            height : 36,
+            width : 263,
+            marginTop : 17,
+            marginLeft : 29,
+            flexDirection : 'row',
+            justifyContent : 'space-between'
+        }}>
+            {/* Cancel Button */}
+            <TouchableOpacity style = {{
+                height : 36,
+                width : 120,
+                backgroundColor : '#294F82',
+                borderRadius : 10,
+                alignItems : 'center',
+                justifyContent : 'space-around',
+            }}>
+                <Text style = {{
+                    fontFamily: FONTS?.regular,
+                    fontWeight : 500,
+                    fontSize : 14,
+                    color: COLORS?.white
+                }}>
+                    Cancel
+                </Text>
+            </TouchableOpacity>
+            {/* Delete Button */}
+            <TouchableOpacity style = {{
+                height : 36,
+                width : 120,
+                borderWidth : 1,
+                borderColor : '#D8D8D8',
+                borderRadius : 10,
+                alignItems : 'center',
+                justifyContent : 'space-around',
+            }}>
+                <Text style = {{
+                    fontFamily: FONTS?.regular,
+                    fontWeight : 500,
+                    fontSize : 14,
+                    color: COLORS?.black
+                }}>
+                    Delete
+                </Text>
+            </TouchableOpacity>
         </View>
     </View> 
   )
