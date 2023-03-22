6a0dafe47fcd64a8e17ab9d7bcaa638d442d1f13
sahelriaz <sahelnriaz@gmail.com>
2023-03-22 20:07:59 +0530

    added image and main layout completed

--- components/home/ErrorPopUp.js
+++ components/home/ErrorPopUp.js
@@ -9,87 +9,100 @@ export default function ErrorPopUp() {
     <View style ={{
         width : 320,
         height : 184,
-        backgroundColor : 'white',
+        backgroundColor : 'red',
         marginTop : 150,
         marginLeft : 35,
         borderRadius : 20,
+        position : 'relative'
     }}>
 
-        {/* Confirm */}
+        {/* bottom */}
         <View style = {{
-            marginTop : 47,
-            alignItems : 'center',
-        }}>
-            <Text style = {{
-                fontFamily: FONTS?.bold, //once check
-                fontWeight : 600,
-                fontSize : 26,
-                lineHeight : 34
+            marginTop : 20,
+            backgroundColor : 'white',
+            height : 164,
+            borderBottomLeftRadius : 10,
+            borderBottomRightRadius : 10,
+            position : 'relative'
 
-            }}>
-                Confirm Deletion
-            </Text>
-            {/* text */}
+        }}>
+            {/* Confirm */}
             <View style = {{
-                marginTop : 8
+                marginTop : 27,
+                alignItems : 'center',
             }}>
                 <Text style = {{
-                    fontFamily: FONTS?.regular,
-                    fontWeight : 300,
-                    fontSize : 14,
-                    lineHeight : 18
+                    fontFamily: FONTS?.bold, //once check
+                    fontWeight : 600,
+                    fontSize : 26,
+                    lineHeight : 34
+
                 }}>
-                    Are you sure you want to delete this session?
+                    Confirm Deletion
                 </Text>
+                {/* text */}
+                <View style = {{
+                    marginTop : 8
+                }}>
+                    <Text style = {{
+                        fontFamily: FONTS?.regular,
+                        fontWeight : 300,
+                        fontSize : 14,
+                        lineHeight : 18
+                    }}>
+                        Are you sure you want to delete this session?
+                    </Text>
+                </View>
+            </View>
+            {/* Buttons */}
+            <View style = {{
+                height : 36,
+                width : 263,
+                marginTop : 17,
+                marginLeft : 29,
+                flexDirection : 'row',
+                justifyContent : 'space-between'
+            }}>
+                {/* Cancel Button */}
+                <TouchableOpacity style = {{
+                    height : 36,
+                    width : 120,
+                    backgroundColor : '#294F82',
+                    borderRadius : 10,
+                    alignItems : 'center',
+                    justifyContent : 'space-around',
+                }}>
+                    <Text style = {{
+                        fontFamily: FONTS?.regular,
+                        fontWeight : 500,
+                        fontSize : 14,
+                        color: COLORS?.white
+                    }}>
+                        Cancel
+                    </Text>
+                </TouchableOpacity>
+                {/* Delete Button */}
+                <TouchableOpacity style = {{
+                    height : 36,
+                    width : 120,
+                    borderWidth : 1,
+                    borderColor : '#D8D8D8',
+                    borderRadius : 10,
+                    alignItems : 'center',
+                    justifyContent : 'space-around',
+                }}>
+                    <Text style = {{
+                        fontFamily: FONTS?.regular,
+                        fontWeight : 500,
+                        fontSize : 14,
+                        color: COLORS?.black
+                    }}>
+                        Delete
+                    </Text>
+                </TouchableOpacity>
             </View>
         </View>
-        {/* Buttons */}
-        <View style = {{
-            height : 36,
-            width : 263,
-            marginTop : 17,
-            marginLeft : 29,
-            flexDirection : 'row',
-            justifyContent : 'space-between'
-        }}>
-            {/* Cancel Button */}
-            <TouchableOpacity style = {{
-                height : 36,
-                width : 120,
-                backgroundColor : '#294F82',
-                borderRadius : 10,
-                alignItems : 'center',
-                justifyContent : 'space-around',
-            }}>
-                <Text style = {{
-                    fontFamily: FONTS?.regular,
-                    fontWeight : 500,
-                    fontSize : 14,
-                    color: COLORS?.white
-                }}>
-                    Cancel
-                </Text>
-            </TouchableOpacity>
-            {/* Delete Button */}
-            <TouchableOpacity style = {{
-                height : 36,
-                width : 120,
-                borderWidth : 1,
-                borderColor : '#D8D8D8',
-                borderRadius : 10,
-                alignItems : 'center',
-                justifyContent : 'space-around',
-            }}>
-                <Text style = {{
-                    fontFamily: FONTS?.regular,
-                    fontWeight : 500,
-                    fontSize : 14,
-                    color: COLORS?.black
-                }}>
-                    Delete
-                </Text>
-            </TouchableOpacity>
-        </View>
+
     </View> 
   )
 }
