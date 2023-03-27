8c51a332866556a00905e7b9132145bef83cd583
Aadhavan <aadhavanlenin@gmail.com>
2023-03-27 19:44:44 +0530

    expo router testing

---
+++ app/home.js
@@ -0,0 +1,9 @@
+import { Link } from 'expo-router'
+
+export default function Home() {
+	return (
+		<View>
+			<Link href='/test'>Home</Link>
+		</View>
+	)
+}
---
+++ app/test.js
@@ -0,0 +1,9 @@
+import { Link } from 'expo-router'
+
+export default function Home() {
+	return (
+		<View>
+			<Link href='/'>Test</Link>
+		</View>
+	)
+}
